f95a177c324f0dcb6a6a1f8d6dc45b917764eb2b
Neihy <neihy.rincon@gmail.com>
2020-05-13 13:47:51 -0400

    adding Confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -149,6 +150,11 @@ storiesOf("InterviewerList", module)
     interviewer={interviewers[0].name}
     onEdit={action('onEdit')}
     onDelete={action('onDelete')}
+  />)
+  .add("Confirm", ()=> <Confirm 
+    message="Delete the appointment?"
+    onConfirm={action('onConfirm')}
+    onCancel={action('onCancel')}
   />);
  
   
