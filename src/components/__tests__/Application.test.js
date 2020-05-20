160ae93ae0ac6f032bd23aa95004bf1629aa6e9b
Neihy <neihy.rincon@gmail.com>
2020-05-20 18:07:59 -0400

    integration test passing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getByAltText, getAllByTestId, getByPlaceholderText } from "@testing-library/react";
 import Application from "components/Application";
 
 afterEach(cleanup);
@@ -14,8 +14,17 @@ describe('Application', () => {
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
-    console.log(prettyDOM(container));
     await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+    console.log(prettyDOM(appointment));
   });
 });
 
